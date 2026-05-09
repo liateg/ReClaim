d10c5077f4819b51a42ec45785f1b367eaa65fa5
liateg <liyategared7@gmail.com>
2026-05-09 23:07:50 +0300

    empy done

--- frontend/lib/features/claims/presentation/screens/claim_screen.dart
+++ frontend/lib/features/claims/presentation/screens/claim_screen.dart
@@ -1,26 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import './claim_empty.dart';
 
 class ClaimsScreen extends StatelessWidget {
   const ClaimsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final claims = ['1', '2', '3', '4'];
+    final claims = ['1', '2', '3', '4', '5'];
 
     return ListView.builder(
       itemCount: claims.length,
       itemBuilder: (context, index) {
         final id = claims[index];
-
-        return ListTile(
-          title: Text('Claim $id'),
-
-          onTap: () {
-            context.go('/claims/$id');
-          },
-        );
+        if (id == '5') {
+          return ListTile(
+            title: const Text('Empty Claims'),
+            onTap: () {
+              context.go('/claims/empty');
+            },
+          );
+        } else {
+          return ListTile(
+            title: Text('Claim $id'),
+            onTap: () {
+              context.go('/claims/$id');
+            },
+          );
+        }
       },
     );
+    
   }
 }
--- frontend/lib/utils/router/app_router.dart
+++ frontend/lib/utils/router/app_router.dart
@@ -5,6 +5,7 @@ import '../layout/main_shell.dart';
 import '../../features/home/home.dart';
 import '../../features/claims/presentation/screens/claim_detail_screen.dart';
 import '../../features/claims/presentation/screens/claim_screen.dart';
+import '../../features/claims/presentation/screens/claim_empty.dart';
 import '../../features/profile/profile_screen.dart';
 import '../../features/settings/settings_screen.dart';
 
@@ -26,22 +27,28 @@ final GoRouter router = GoRouter(
         ),
 
         GoRoute(
-          path: '/items',
+          path: '/claims',
           builder: (context, state) => const ClaimsScreen(),
 
-          routes: [
-            GoRoute(
-              path: ':id',
-              builder: (context, state) {
-                final id = state.pathParameters['id']!;
-                return ClaimDetailScreen(claimId: id);
-              },
-            ),
-          ],
+      routes: [
+  GoRoute(
+    path: 'empty',
+    builder: (context, state) {
+      return const ClaimEmptyScreen();
+    },
+  ),
+  GoRoute(
+    path: ':id',
+    builder: (context, state) {
+      final id = state.pathParameters['id']!;
+      return ClaimDetailScreen(claimId: id);
+    },
+  ),
+],
         ),
 
         GoRoute(
-          path: '/claims',
+          path: '/items',
           builder: (context, state) => const SettingsScreen(),
         ),
       ],
