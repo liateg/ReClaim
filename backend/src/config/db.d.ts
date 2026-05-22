4e501831bf2a5d7c02b1c4cea3644c49dc0c9303
braveset14 <braveset14@gmail.com>
2026-05-22 12:23:28 +0300

    clean up

--- backend/src/config/db.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const pool: import("pg").Pool;
-//# sourceMappingURL=db.d.ts.map
--- backend/src/types/auth.ts
+++
@@ -1,8 +0,0 @@
-export type UserRole = "user" | "admin";
-
-export interface AuthTokenPayload {
-  id: number;
-  full_name: string;
-  email: string;
-  role: UserRole;
-}
