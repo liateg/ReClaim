ba1a3aa6b08a44ed98aa3db4f9e2cca59e98957b
liateg <liyategared7@gmail.com>
2026-05-23 21:58:23 +0300

    emultor ip

--- frontend/lib/core/session/services/auth_service.dart
+++ frontend/lib/core/session/services/auth_service.dart
@@ -5,7 +5,7 @@ class AuthService {
   final Dio _dio = Dio();
   final FlutterSecureStorage _storage = const FlutterSecureStorage();
 
-  static const String baseUrl = 'http://localhost:3000';
+  static const String baseUrl = 'http://10.0.2.2:3000';
   static String? _testToken;
 
   static void setTestToken(String? token) => _testToken = token;
